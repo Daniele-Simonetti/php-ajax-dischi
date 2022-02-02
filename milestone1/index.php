633c55c79beff74484f526e8a08adc97e83cb497
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2022-02-02 16:28:35 +0100

    end milestone1

--- milestone1/index.php
+++ milestone1/index.php
@@ -7,6 +7,9 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Asap+Condensed:wght@400;500;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/style.css">
   <title>spotify</title>
 </head>
@@ -22,6 +25,19 @@
   <?php 
   include_once __DIR__ . '/server/data.php'; 
   ?>
+  <main>
+    <div class="row row-col-5">
+      <?php
+      foreach ($discs as $disc) { ?>
+        <div class="card col text-center">
+          <img src="<?= $disc['img']?>" alt="<?= $disc['title'] ?>">
+          <h2 class="fs-5 fw-bold mt-3 mb-4"><?= $disc['title'] ?></h2>
+          <h3 class="fs-6"><?= $disc['author'] ?></h3>
+          <h4 class="fs-6"><?= $disc['year'] ?></h4>
+        </div>
+      <?php } ?>
+    </div>
+  </main>
 
   <!-- footer -->
   <!-- includo il mio componente footer -->
