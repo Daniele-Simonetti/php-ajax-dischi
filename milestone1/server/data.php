13ca56bb1f5708ec7ecec70d69a4a681df7e6eff
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2022-02-02 15:39:49 +0100

    create datas

---
+++ milestone1/server/data.php
@@ -0,0 +1,54 @@
+<?php 
+$discs = [
+  [
+    'title' => 'New Jersey',
+    'author' => 'Bon Jovi',
+    'year' => 1998, 
+  ],
+  [
+    'title' => 'Live at Wembley 86',
+    'author' => 'Queen',
+    'year' => 1992, 
+  ],
+  [
+    'title' => 'Ten\'s Summoner\'s Tales',
+    'author' => 'Sting',
+    'year' => 1993, 
+  ],
+  [
+    'title' => 'Steve Gadd Band',
+    'author' => 'Sytave Gadd band',
+    'year' => 2018, 
+  ],
+  [
+    'title' => 'Brave New World',
+    'author' => 'Iron Maiden',
+    'year' => 2000, 
+  ],
+  [
+    'title' => 'One More Car, One More Raider',
+    'author' => 'Eric Clapton',
+    'year' => 2002, 
+  ],
+  [
+    'title' => 'Made In Japan',
+    'author' => 'Deep Purple',
+    'year' => 1972, 
+  ],
+  [
+    'title' => 'And Justice For All',
+    'author' => 'Matallica',
+    'year' => 1988, 
+  ],
+  [
+    'title' => 'Hard Wired',
+    'author' => 'Dave Weckl',
+    'year' => 1994, 
+  ],
+  [
+    'title' => 'Bad',
+    'author' => 'Nichael Jackson',
+    'year' => 1987, 
+  ],
+];
+?>
